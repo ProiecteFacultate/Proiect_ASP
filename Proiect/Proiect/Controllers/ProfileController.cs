be5e5d1c6fe0b03dde90194d1404eb36ced297b3
Mihai Razvan <MIHAIRAZVAN03@YAHOO.COM>
2022-12-11 22:57:50 +0200

    .

---
+++ Proiect/Proiect/Controllers/ProfileController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Proiect.Data;
+using Proiect.Models;
+using System.Data;
+using System.Diagnostics;
+
+
+namespace Proiect.Controllers
+{
+    public class ProfileController : Controller
+    {
+        private readonly ApplicationDbContext db;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        public ProfileController(
+        ApplicationDbContext context,
+        UserManager<ApplicationUser> userManager,
+        RoleManager<IdentityRole> roleManager
+        )
+        {
+            db = context;
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+
+        public IActionResult Show()
+        {
+            ViewBag.CurrentUser = _userManager.GetUserName(User);
+           
+            return View();
+        }
+    }
+}
--- Proiect/Proiect/Models/ApplicationUser.cs
+++ Proiect/Proiect/Models/ApplicationUser.cs
@@ -4,5 +4,7 @@ namespace Proiect.Models
 {
     public class ApplicationUser : IdentityUser
     {
+        public string? ProfileUsername { get; set; }
+        public string? Description { get; set; }
     }
 }
