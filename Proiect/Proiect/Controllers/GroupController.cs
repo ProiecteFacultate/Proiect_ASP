cd03aeb3a7b2444015fd8a83a0366ff3b629cc6b
Mihai Razvan <MIHAIRAZVAN03@YAHOO.COM>
2023-01-09 11:46:26 +0200

    final

--- Proiect/Proiect/Controllers/GroupController.cs
+++ Proiect/Proiect/Controllers/GroupController.cs
@@ -117,6 +117,7 @@ namespace Proiect.Controllers
                         }
 
                         newGroup.GroupImage = databaseFileName;
+
                         db.SaveChanges();
                     }
 
